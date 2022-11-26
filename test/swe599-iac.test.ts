6f6edadeeded584ea291467199e6ddbb2fac158e
onurdemirkale <onurdemirkale@protonmail.com>
2022-11-26 12:02:33 +0300

    Initialized a CDK app with typescript as the language

---
+++ test/swe599-iac.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Swe599Iac from '../lib/swe599-iac-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/swe599-iac-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Swe599Iac.Swe599IacStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
