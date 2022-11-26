4a9a19a8dafff25987363fd051cb2a45a22db857
onurdemirkale <onurdemirkale@protonmail.com>
2022-11-26 12:35:56 +0300

    Implemented client stack creation

--- bin/swe599-iac.ts
+++ bin/swe599-iac.ts
@@ -1,5 +1,15 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
+import ClientStack from '../lib/client';
+import stackConstant from '../constants/stackConstant';
 
 const app = new cdk.App();
+
+// Create the client stack which hosts the React web application
+new ClientStack(app, stackConstant.CLIENT_STACK_NAME, {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+});
