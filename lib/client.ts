cdc389f2b96303412053dc75d377326f2da2e29d
onurdemirkale <onurdemirkale@protonmail.com>
2022-11-26 18:17:24 +0300

    Refactored the client resources into constructor to allow variables

--- lib/client.ts
+++ lib/client.ts
@@ -14,65 +14,65 @@ import clientConstant from '../constants/clientConstant';
 export default class Client extends Stack {
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
+
+    // Create an S3 Bucket for static web application hosting
+    const bucket = new s3.Bucket(this, clientConstant.BUCKET_NAME, {
+      bucketName: clientConstant.BUCKET_NAME,
+      encryption: s3.BucketEncryption.UNENCRYPTED,
+      websiteIndexDocument: 'index.html',
+      publicReadAccess: true,
+      removalPolicy: RemovalPolicy.DESTROY, // Enables automatic deletion during `cdk destroy`
+      autoDeleteObjects: true,
+    });
+
+    // Authenticate to GitHub repository through codebuild using credentials stored in secrets manager
+    const codebuildCredentials = new codebuild.GitHubSourceCredentials(
+      this,
+      clientConstant.CODEBUILD_CREDENTIALS_NAME,
+      {
+        accessToken: SecretValue.secretsManager(
+          clientConstant.GITHUB_TOKEN_NAME,
+          {jsonField: 'accessToken'}
+        ),
+      }
+    );
+
+    // Create a CodeBuild Project and deploy the artifact to previously created S3 Bucket
+    const codeBuildProject = new codebuild.Project(
+      this,
+      clientConstant.CODEBUILD_PROJECT_NAME,
+      {
+        projectName: clientConstant.CODEBUILD_PROJECT_NAME,
+        source: codebuild.Source.gitHub({
+          repo: clientConstant.REPOSITORY_NAME,
+          owner: clientConstant.REPOSITORY_OWNER,
+          webhook: true,
+          webhookFilters: [
+            codebuild.FilterGroup.inEventOf(
+              codebuild.EventAction.PULL_REQUEST_MERGED
+            ).andBranchIs(clientConstant.DEPLOYMENT_BRANCH),
+          ],
+        }),
+        buildSpec: codebuild.BuildSpec.fromObject({
+          version: '0.2',
+          phases: {
+            install: {
+              commands: 'npm install',
+            },
+            build: {
+              commands: 'npm run build',
+            },
+          },
+          artifacts: {
+            files: '**/*',
+          },
+        }),
+        environment: {
+          buildImage: codebuild.LinuxBuildImage.AMAZON_LINUX_2_4,
+          computeType: codebuild.ComputeType.SMALL,
+        },
+        checkSecretsInPlainTextEnvVariables: false,
+      }
+    );
   }
-
-  // Create an S3 Bucket for static web application hosting
-  bucket = new s3.Bucket(this, clientConstant.BUCKET_NAME, {
-    bucketName: clientConstant.BUCKET_NAME,
-    encryption: s3.BucketEncryption.UNENCRYPTED,
-    websiteIndexDocument: 'index.html',
-    publicReadAccess: true,
-    removalPolicy: RemovalPolicy.DESTROY, // Enables automatic deletion during `cdk destroy`
-    autoDeleteObjects: true,
-  });
-
-  // Authenticate to GitHub repository through codebuild using credentials stored in secrets manager
-  codebuildCredentials = new codebuild.GitHubSourceCredentials(
-    this,
-    clientConstant.CODEBUILD_CREDENTIALS_NAME,
-    {
-      accessToken: SecretValue.secretsManager(
-        clientConstant.GITHUB_TOKEN_NAME,
-        {jsonField: 'accessToken'}
-      ),
-    }
-  );
-
-  // Create a CodeBuild Project and deploy the artifact to previously created S3 Bucket
-  codeBuildProject = new codebuild.Project(
-    this,
-    clientConstant.CODEBUILD_PROJECT_NAME,
-    {
-      projectName: clientConstant.CODEBUILD_PROJECT_NAME,
-      source: codebuild.Source.gitHub({
-        repo: clientConstant.REPOSITORY_NAME,
-        owner: clientConstant.REPOSITORY_OWNER,
-        webhook: true,
-        webhookFilters: [
-          codebuild.FilterGroup.inEventOf(
-            codebuild.EventAction.PULL_REQUEST_MERGED
-          ).andBranchIs(clientConstant.DEPLOYMENT_BRANCH),
-        ],
-      }),
-      buildSpec: codebuild.BuildSpec.fromObject({
-        version: '0.2',
-        phases: {
-          install: {
-            commands: 'npm install',
-          },
-          build: {
-            commands: 'npm run build',
-          },
-        },
-        artifacts: {
-          files: '**/*',
-        },
-      }),
-      environment: {
-        buildImage: codebuild.LinuxBuildImage.AMAZON_LINUX_2_4,
-        computeType: codebuild.ComputeType.SMALL,
-      },
-      checkSecretsInPlainTextEnvVariables: false,
-    }
-  );
 }
