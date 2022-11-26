3244968c20f25ac6dda91164d00de85e38078571
onurdemirkale <onurdemirkale@protonmail.com>
2022-11-26 12:37:42 +0300

    Implemented environment variables validation

--- bin/swe599-iac.ts
+++ bin/swe599-iac.ts
@@ -6,6 +6,11 @@ import stackConstant from '../constants/stackConstant';
 
 const app = new cdk.App();
 
+// Validate the environment variables
+if (!process.env.CDK_DEFAULT_ACCOUNT || !process.env.CDK_DEFAULT_REGION) {
+  throw new Error('CDK environment variables are not defined in the CLI.');
+}
+
 // Create the client stack which hosts the React web application
 new ClientStack(app, stackConstant.CLIENT_STACK_NAME, {
   env: {
