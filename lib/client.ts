ca638318ad45620ca8541447d8ebbb1066d03816
onurdemirkale <onurdemirkale@protonmail.com>
2022-11-26 12:27:41 +0300

    Implemented the S3 bucket to be used for static website hosting

--- lib/client.ts
+++ lib/client.ts
@@ -1,8 +1,20 @@
-import {Stack, StackProps} from 'aws-cdk-lib';
+import {Stack, StackProps, RemovalPolicy} from 'aws-cdk-lib';
 import {Construct} from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import clientConstant from '../constants/clientConstant';
 
 export default class Client extends Stack {
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
   }
+
+  // Create an S3 Bucket for static web application hosting
+  bucket = new s3.Bucket(this, clientConstant.BUCKET_NAME, {
+    bucketName: clientConstant.BUCKET_NAME,
+    encryption: s3.BucketEncryption.UNENCRYPTED,
+    websiteIndexDocument: 'index.html',
+    publicReadAccess: true,
+    removalPolicy: RemovalPolicy.DESTROY, // Enables automatic deletion during `cdk destroy`
+    autoDeleteObjects: true,
+  });
 }
