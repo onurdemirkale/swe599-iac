7277fd818c9c67884a1a0c1139840b07d564ffed
onurdemirkale <onurdemirkale@protonmail.com>
2022-12-10 11:23:00 +0300

    Refactored and updated the stack constants

--- constants/stackConstant.ts
+++ constants/stackConstant.ts
@@ -1,5 +1,6 @@
-const stackConstant = {
+const STACK = {
   CLIENT_STACK_NAME: 'swe599-client-stack',
+  VPC_STACK_NAME: 'swe599-vpc-stack',
 };
 
-export default stackConstant;
+export default STACK;
