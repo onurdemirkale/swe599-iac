8badbcf551076f574cf8e6229681df16f6155f98
onurdemirkale <onurdemirkale@protonmail.com>
2022-12-10 22:22:41 +0300

    Renamed the interface used for Lambda constants

---
+++ interfaces/LambdaConstant.ts
@@ -0,0 +1,13 @@
+import {aws_lambda} from 'aws-cdk-lib';
+
+// The LambdaConstnat interface is used to define constants, thus its
+// attributes are written in upper case
+interface LambdaConstant {
+  NAME: string;
+  ARCHITECTURE: aws_lambda.Architecture;
+  CODEBUILD_ON_COMMIT_BRANCHES: string[];
+  MEMORY_SIZE: number;
+  TYPE: ('GET' | 'POST' | 'PUT' | 'DELETE' | 'AUTH')[];
+}
+
+export default LambdaConstant;
