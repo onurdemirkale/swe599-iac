bfe4f7e2916c809289b78285b9c7da67ee27d67e
onurdemirkale <onurdemirkale@protonmail.com>
2022-12-10 11:23:50 +0300

    Refactored the bin file and included the VPC stack

--- bin/swe599-iac.ts
+++ bin/swe599-iac.ts
@@ -1,17 +1,32 @@
 #!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import ClientStack from '../lib/client';
-import stackConstant from '../constants/stackConstant';
 
-const app = new cdk.App();
+// CDK
+import {App} from 'aws-cdk-lib';
+
+// Constants
+import STACK from '../constants/stackConstant';
+
+// Stacks
+import VpcStack from '../lib/backend/vpcStack';
+import ClientStack from '../lib/clientStack';
+
+const app = new App();
 
 // Validate the environment variables
 if (!process.env.CDK_DEFAULT_ACCOUNT || !process.env.CDK_DEFAULT_REGION) {
   throw new Error('CDK environment variables are not defined in the CLI.');
 }
 
+// Create the VPC stack which provides the virtual network for the resources
+new VpcStack(app, STACK.VPC_STACK_NAME, {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+});
+
 // Create the client stack which hosts the React web application
-new ClientStack(app, stackConstant.CLIENT_STACK_NAME, {
+new ClientStack(app, STACK.CLIENT_STACK_NAME, {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
