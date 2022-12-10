ee5c5a16c609265327c5219c38d96ee3b4aed36c
onurdemirkale <onurdemirkale@protonmail.com>
2022-12-10 12:15:33 +0300

    Renamed LambdaInterface attributes for better readability

--- interfaces/LambdaInterface.ts
+++ interfaces/LambdaInterface.ts
@@ -1,8 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 
 interface LambdaInterface {
-  LAMBDA_NAME: string;
-  LAMBDA_ARCHITECTURE: cdk.aws_lambda.Architecture;
+  NAME: string;
+  ARCHITECTURE: cdk.aws_lambda.Architecture;
   CODEBUILD_ON_COMMIT_BRANCHES: string[];
   MEMORY_SIZE: number;
   TYPE: ('GET' | 'POST' | 'PUT' | 'DELETE' | 'AUTH')[];
