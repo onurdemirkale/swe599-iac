ab17939ab09183aa76ad43d2c0a97fdab4a84770
onurdemirkale <onurdemirkale@protonmail.com>
2022-12-10 22:21:46 +0300

    Refactored stackConstants into globalConstants

---
+++ constants/globalConstant.ts
@@ -0,0 +1,4 @@
+const CLIENT_STACK_NAME = 'swe599-client-stack';
+const VPC_STACK_NAME = 'swe599-vpc-stack';
+
+export {CLIENT_STACK_NAME, VPC_STACK_NAME};
--- constants/stackConstant.ts
+++
@@ -1,6 +0,0 @@
-const STACK = {
-  CLIENT_STACK_NAME: 'swe599-client-stack',
-  VPC_STACK_NAME: 'swe599-vpc-stack',
-};
-
-export default STACK;
