c31f4d1497149fc4833b6360556201388f07bb39
onurdemirkale <onurdemirkale@protonmail.com>
2022-12-18 02:08:05 +0300

    Included the backend stack in the bin file

--- bin/swe599-iac.ts
+++ bin/swe599-iac.ts
@@ -12,6 +12,7 @@ import {
 // Stacks
 import VpcStack from '../lib/backend/vpcStack';
 import ClientStack from '../lib/frontend/clientStack';
+import BackendStack from '../lib/backend/backendStack';
 
 const app = new App();
 
@@ -21,7 +22,7 @@ if (!process.env.CDK_DEFAULT_ACCOUNT || !process.env.CDK_DEFAULT_REGION) {
 }
 
 // Create the VPC stack which provides the virtual network for the resources
-new VpcStack(app, VPC_STACK_NAME, {
+const vpcStack = new VpcStack(app, VPC_STACK_NAME, {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
@@ -34,4 +35,13 @@ new ClientStack(app, CLIENT_STACK_NAME, {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
   },
+});
+
+// Create the client stack which hosts the React web application
+new BackendStack(app, 'backendStack', {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+  vpc: vpcStack.vpc,
 });
