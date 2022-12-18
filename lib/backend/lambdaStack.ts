067db30a8996bce475366f57ab6e2a88f0c61397
onurdemirkale <onurdemirkale@protonmail.com>
2022-12-18 16:50:12 +0300

    Implemented environment variables check

--- lib/backend/lambdaStack.ts
+++ lib/backend/lambdaStack.ts
@@ -10,5 +10,12 @@ import LambdaStackProps from '../interfaces/BackendStackProps';
 export default class LambdaStack extends Stack {
   constructor(scope: Construct, id: string, props: LambdaStackProps) {
     super(scope, id, props);
+
+    // Environment variable checks
+    if (!process.env.CDK_DEFAULT_ACCOUNT || !process.env.CDK_DEFAULT_REGION) {
+      throw new Error(
+        'CDK credentials are not defined the environment variables!'
+      );
+    }
   }
 }
