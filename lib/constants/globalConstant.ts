95015c8547e0588addbe8f6fb99d08aa8ef7c817
onurdemirkale <onurdemirkale@protonmail.com>
2022-12-18 17:20:56 +0300

    Defined the backend stack name constant

--- lib/constants/globalConstant.ts
+++ lib/constants/globalConstant.ts
@@ -1,4 +1,5 @@
 const CLIENT_STACK_NAME = 'swe599-client-stack';
 const VPC_STACK_NAME = 'swe599-vpc-stack';
+const BACKEND_STACK_NAME = 'swe599-backend-stack';
 
-export {CLIENT_STACK_NAME, VPC_STACK_NAME};
+export {CLIENT_STACK_NAME, VPC_STACK_NAME, BACKEND_STACK_NAME};
