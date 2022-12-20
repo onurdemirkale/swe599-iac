07b727b0ad60d16c512bf26c3210307bc017b02b
onurdemirkale <onurdemirkale@protonmail.com>
2022-12-20 22:59:21 +0300

    Implemented the props interface for the API Gateway stack

--- lib/interfaces/ApiGatewayStackProps.ts
+++ lib/interfaces/ApiGatewayStackProps.ts
@@ -1,7 +1,15 @@
 import {StackProps} from 'aws-cdk-lib';
 
 interface ApiGatewayStackProps extends StackProps {
-  null: any;
+  apiGatewayDescription: string;
+  apiGatewayStageName: string;
+  apiGatewayAllowedMethods: [
+    'OPTIONS' | 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE'
+  ];
+  apiGatewayAllowHeaders: string[];
+  apiGatewayAllowMethods: string[];
+  apiGatewayAllowCredentials: boolean;
+  apiGatewayAllowOrigins: string[];
 }
 
 export {ApiGatewayStackProps};
