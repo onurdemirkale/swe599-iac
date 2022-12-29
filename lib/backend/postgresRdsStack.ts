23a3119b318c9994cd6ff05376a8c9320ab32610
onurdemirkale <onurdemirkale@protonmail.com>
2022-12-30 00:52:28 +0300

    Initialize the Postgres RDS stack class

--- lib/backend/postgresRdsStack.ts
+++ lib/backend/postgresRdsStack.ts
@@ -8,3 +8,16 @@ import * as ec2 from 'aws-cdk-lib/aws-ec2';
 // Interfaces
 import {PostgresRdsStackProps} from '../interfaces/PostgresRdsStackProps';
 
+export default class PostgresRdsStack extends Stack {
+  props: PostgresRdsStackProps;
+
+  // Set attributes
+  private rdsSecurityGroup: ec2.SecurityGroup;
+
+  constructor(scope: Construct, id: string, props: PostgresRdsStackProps) {
+    super(scope, id, props);
+
+    this.props = props;
+  }
+
+}
