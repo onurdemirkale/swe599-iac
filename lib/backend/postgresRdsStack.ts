ced7f29b98f8bddd7d502b227b66934a33c4ae0f
onurdemirkale <onurdemirkale@protonmail.com>
2022-12-30 00:51:33 +0300

    Import postgres RDS stack props interface

--- lib/backend/postgresRdsStack.ts
+++ lib/backend/postgresRdsStack.ts
@@ -4,3 +4,7 @@ import {Construct} from 'constructs';
 import * as secretmanager from 'aws-cdk-lib/aws-secretsmanager';
 import * as rds from 'aws-cdk-lib/aws-rds';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
+
+// Interfaces
+import {PostgresRdsStackProps} from '../interfaces/PostgresRdsStackProps';
+
