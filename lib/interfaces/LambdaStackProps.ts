93347111a8e9a41d03565f19a2ce0c50c94288fd
onurdemirkale <onurdemirkale@protonmail.com>
2022-12-30 00:33:26 +0300

    Update LambdaStackProps interface to reflect changes on Lambda integration

--- lib/interfaces/LambdaStackProps.ts
+++ lib/interfaces/LambdaStackProps.ts
@@ -1,7 +1,13 @@
+import {aws_lambda} from 'aws-cdk-lib';
 import {StackProps} from 'aws-cdk-lib';
 
 interface LambdaStackProps extends StackProps {
   lambdaFunctionName: string;
+  lambdaArchiteture: aws_lambda.Architecture;
+  lambdaMemorySize: number;
+  httpMethod: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'AUTH';
+  apiGatewayRootPath: string;
+  apiGatewayResourcePath: string | null;
   sourceCodeRepositoryName: string;
   sourceCodeRepositoryOwner: string;
   sourceCodeRepositoryBranch: string;
