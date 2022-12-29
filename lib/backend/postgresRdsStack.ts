21517852551e9b24972fe91d00af835dc980c89b
onurdemirkale <onurdemirkale@protonmail.com>
2022-12-30 00:51:11 +0300

    Import necessary CDK libraries for postgres RDS stack

---
+++ lib/backend/postgresRdsStack.ts
@@ -0,0 +1,6 @@
+// CDK
+import {RemovalPolicy, Stack} from 'aws-cdk-lib';
+import {Construct} from 'constructs';
+import * as secretmanager from 'aws-cdk-lib/aws-secretsmanager';
+import * as rds from 'aws-cdk-lib/aws-rds';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
