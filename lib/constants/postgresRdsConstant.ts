b8b5f6fbf73b4279af7c235b8cbb6099705e5190
onurdemirkale <onurdemirkale@protonmail.com>
2023-01-07 03:45:42 +0300

    Implement postgres RDS constants interface and the constants, separate from RDS stack props

---
+++ lib/constants/postgresRdsConstant.ts
@@ -0,0 +1,16 @@
+import {PostgresRdsConstant} from '../interfaces/PostgresRdsConstants';
+
+const POSTGRES_RDS: PostgresRdsConstant = {
+  databaseId: 'swe599-rds',
+  databaseSecurityGroupId: 'swe599-rds-security-group',
+  subnetGroupId: 'swe599-rds-subnet-group',
+  bastionHostId: 'swe599-rds-bastion-host',
+  bastionHostInstanceName: 'swe599-rds-bastion-host-instance',
+  bastionHostSecurityGroupId: 'swe599-rds-bastion-host-security-group',
+  secretId: 'swe599-rds-credentials',
+  secretName: 'swe599-rds-credentials',
+  secretUsername: 'postgres',
+  secretGenerateStringKey: 'password',
+};
+
+export {POSTGRES_RDS};
---
+++ lib/interfaces/PostgresRdsConstant.ts
@@ -0,0 +1,14 @@
+interface PostgresRdsConstant {
+  databaseId: string;
+  databaseSecurityGroupId: string;
+  subnetGroupId: string;
+  bastionHostId: string;
+  bastionHostInstanceName: string;
+  bastionHostSecurityGroupId: string;
+  secretId: string;
+  secretName: string;
+  secretUsername: string;
+  secretGenerateStringKey: string;
+}
+
+export {PostgresRdsConstant};
