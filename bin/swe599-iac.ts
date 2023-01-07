f97b34dc81b3bfc397aa5923d898c1e372d362af
onurdemirkale <onurdemirkale@protonmail.com>
2023-01-07 04:03:08 +0300

    Implement the posgtres RDS stack in the root file

--- bin/swe599-iac.ts
+++ bin/swe599-iac.ts
@@ -8,12 +8,15 @@ import {
   VPC_STACK_NAME,
   CLIENT_STACK_NAME,
   BACKEND_STACK_NAME,
+  POSTGRES_RDS_STACK_NAME,
 } from '../lib/constants/globalConstant';
+import {POSTGRES_RDS} from '../lib/constants/postgresRdsConstant';
 
 // Stacks
 import VpcStack from '../lib/backend/vpcStack';
 import ClientStack from '../lib/frontend/clientStack';
 import BackendStack from '../lib/backend/backendStack';
+import PostgresRdsStack from '../lib/backend/postgresRdsStack';
 
 const app = new App();
 
@@ -37,6 +40,26 @@ new ClientStack(app, CLIENT_STACK_NAME, {
     region: process.env.CDK_DEFAULT_REGION,
   },
 });
+
+const postgresRdsStack = new PostgresRdsStack(app, POSTGRES_RDS_STACK_NAME, {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+  vpc: vpcStack.vpc,
+  databaseId: POSTGRES_RDS.databaseId,
+  databaseSecurityGroupId: POSTGRES_RDS.databaseSecurityGroupId,
+  subnetGroupId: POSTGRES_RDS.subnetGroupId,
+  bastionHostId: POSTGRES_RDS.bastionHostId,
+  bastionHostInstanceName: POSTGRES_RDS.bastionHostInstanceName,
+  bastionHostSecurityGroupId: POSTGRES_RDS.bastionHostSecurityGroupId,
+  secretId: POSTGRES_RDS.secretId,
+  secretName: POSTGRES_RDS.secretName,
+  secretUsername: POSTGRES_RDS.secretUsername,
+  secretGenerateStringKey: POSTGRES_RDS.secretGenerateStringKey,
+});
+
+const postgresRdsInstance = postgresRdsStack.createPostgresDatabase();
 
 new BackendStack(app, BACKEND_STACK_NAME, {
   env: {
