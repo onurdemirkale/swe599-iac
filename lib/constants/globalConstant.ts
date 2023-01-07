73679ddf608c834bae2e120008a88e6caf6b92d5
onurdemirkale <onurdemirkale@protonmail.com>
2023-01-07 03:45:59 +0300

    Update global constants to include a name for the postgres RDS stack

--- lib/constants/globalConstant.ts
+++ lib/constants/globalConstant.ts
@@ -1,5 +1,11 @@
 const CLIENT_STACK_NAME = 'swe599-client-stack';
 const VPC_STACK_NAME = 'swe599-vpc-stack';
 const BACKEND_STACK_NAME = 'swe599-backend-stack';
+const POSTGRES_RDS_STACK_NAME = 'swe599-postgres-rds-stack';
 
-export {CLIENT_STACK_NAME, VPC_STACK_NAME, BACKEND_STACK_NAME};
+export {
+  CLIENT_STACK_NAME,
+  VPC_STACK_NAME,
+  BACKEND_STACK_NAME,
+  POSTGRES_RDS_STACK_NAME,
+};
