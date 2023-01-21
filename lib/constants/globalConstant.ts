41947ec418aa29d80c58876bfd31cc20426355e2
onurdemirkale <onurdemirkale@protonmail.com>
2023-01-21 17:35:37 +0300

    update global constants for the secret stack

--- lib/constants/globalConstant.ts
+++ lib/constants/globalConstant.ts
@@ -2,10 +2,12 @@ const CLIENT_STACK_NAME = 'swe599-client-stack';
 const VPC_STACK_NAME = 'swe599-vpc-stack';
 const BACKEND_STACK_NAME = 'swe599-backend-stack';
 const POSTGRES_RDS_STACK_NAME = 'swe599-postgres-rds-stack';
+const SECRET_STACK_NAME = 'swe599-secret-stack';
 
 export {
   CLIENT_STACK_NAME,
   VPC_STACK_NAME,
   BACKEND_STACK_NAME,
   POSTGRES_RDS_STACK_NAME,
+  SECRET_STACK_NAME,
 };
